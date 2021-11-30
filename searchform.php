4af93084ae2027c6569b3f19776101ed63a0eb69
Davi Manoel <sondavifaco@gmail.com>
2021-11-30 16:27:27 -0300

    top form css

--- searchform.php
+++ searchform.php
@@ -1,3 +1,7 @@
 <form action="/" method="get">
-    <input type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
+    <div class="top_form">
+        <input type="text" name="s" placeholder="<?php esc_html_e( 'Busque um produto ...', 'sekiama' ); ?>" id="search" value="<?php the_search_query(); ?>" />
+
+        <span class="btn_search_form"><input type="image" alt="<?php esc_html_e( 'Search Form', 'sekiama' ); ?>" src="<?php echo get_template_directory_uri(); ?>/dist/images/search-icon.png" /></span>
+    </div>
 </form>
--- template-parts/top/search-top.php
+++ template-parts/top/search-top.php
@@ -3,11 +3,15 @@
 </div>
 
 <div class="col-lg-4">
+
   <div>
     <img src="<?php echo get_template_directory_uri(); ?>/dist/images/bem-vindo.svg">
-    <p>Entre ou cadastre-se</p>
-    <p>BEM VINDO!</p>
+    <div>
+      <p>Entre ou cadastre-se</p>
+      <p>BEM VINDO!</p>
+    </div>
   </div>
+
   <ul>
     <li>
       <img src="<?php echo get_template_directory_uri(); ?>/dist/images/heart-icon.svg">
