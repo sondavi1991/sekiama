cbc69123267be6fe336e97f4719eecef91e51f04
Davi Manoel <sondavifaco@gmail.com>
2021-12-05 18:50:47 -0300

    Adding flexslider library.

--- config/scripts.php
+++ config/scripts.php
@@ -6,6 +6,10 @@ function sekiamaScripts() {
     //CSS
     wp_enqueue_style('reset', get_template_directory_uri().'/dist/css/reset.css', array(), '1.0', 'all');
     wp_enqueue_style('template', get_template_directory_uri().'/dist/css/style.css', array(), '1.0', 'all');
+    wp_enqueue_style('flexslider', get_template_directory_uri().'/dist/css/flexslider.css', array(), '1.0', 'all');
+
+    //JS
+    wp_enqueue_script('flexsliderjs', get_template_directory_uri(). '/dist/js/jquery.flexslider-min.js', array('jquery'), null, true);
     
 }
 
