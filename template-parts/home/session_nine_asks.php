cca0bc6131568257f8e541f0226fe493dce1c699
Davi Manoel <sondavifaco@gmail.com>
2021-12-12 20:04:25 -0300

    Question-answered session.

--- template-parts/home/session_nine_asks.php
+++ template-parts/home/session_nine_asks.php
@@ -1,4 +1,41 @@
 <section class="container inner_space_section">
   <span class="session_icon"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/icon_details.svg"></span>
-  <h2 class="big_title"><?php esc_html_e( 'Perguntas Frequentes', 'sekiama' ); ?></h2>
+  <h2 class="big_title bottom29"><?php esc_html_e( 'Perguntas Frequentes', 'sekiama' ); ?></h2>
+
+  <div class="row">
+    <div class="col-lg-6">
+      <div class="wrap_ask">
+        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/perguntas_icon.png">
+        <h3><?php esc_html_e( 'Os produtos são 100% da Amazônia?', 'sekiama' ); ?></h3>
+        <p><?php esc_html_e( 'Fundada pela Nutricionista e Chef Funcional Samila Seki, a Sekiama desenvolve tudo com muito respaldo profissional, qualidade e amor.', 'sekiama' ); ?></p>
+      </div>
+    </div>
+    <div class="col-lg-6">
+      <div class="wrap_ask">
+        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/perguntas_icon.png">
+        <h3><?php esc_html_e( 'Vocês entregam para todo o Brasil?', 'sekiama' ); ?></h3>
+        <p><?php esc_html_e( 'Fundada pela Nutricionista e Chef Funcional Samila Seki, a Sekiama desenvolve tudo com muito respaldo profissional, qualidade e amor.', 'sekiama' ); ?></p>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-6">
+      <div class="wrap_ask">
+        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/perguntas_icon.png">
+        <h3><?php esc_html_e( 'Qual é a média de preço?', 'sekiama' ); ?></h3>
+        <p><?php esc_html_e( 'Fundada pela Nutricionista e Chef Funcional Samila Seki, a Sekiama desenvolve tudo com muito respaldo profissional, qualidade e amor.', 'sekiama' ); ?></p>
+      </div>
+    </div>
+    <div class="col-lg-6">
+      <div class="wrap_ask">
+        <img src="<?php echo get_template_directory_uri(); ?>/dist/images/perguntas_icon.png">
+        <h3><?php esc_html_e( 'Como posso encontrar a Sekiama na minha localidade?', 'sekiama' ); ?></h3>
+        <p><?php esc_html_e( 'Fundada pela Nutricionista e Chef Funcional Samila Seki, a Sekiama desenvolve tudo com muito respaldo profissional, qualidade e amor.', 'sekiama' ); ?></p>
+      </div>
+    </div>
+  </div>
+
+  <a class="wrapper_questions" href="#">
+    <button class="btn_questions"><?php esc_html_e( 'Tirar mais', 'sekiama' ); ?><br><?php esc_html_e( 'dúvidas', 'sekiama' ); ?></button>
+  </a>
 </section>
